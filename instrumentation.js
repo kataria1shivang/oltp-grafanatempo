cc822f52ce981bc9a19a1e6ada340daceb480a02
kataria1shivang <shivang.kataria.in@gmail.com>
2024-02-12 17:04:07 +0530

    first commit

---
+++ instrumentation.js
@@ -0,0 +1,33 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
+// Configure the OTLP HTTP exporter
+const otlpExporter = new OTLPTraceExporter({
+    url: 'http://host.docker.internal:4318/v1/traces', 
+  });
+
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: 'nodejs', 
+});
+
+const sdk = new NodeSDK({
+  resource: resource,
+  traceExporter: otlpExporter,
+  spanProcessor: new BatchSpanProcessor(otlpExporter, {
+    scheduledDelayMillis: 5000, // Adjust as needed
+    maxQueueSize: 100, // Adjust based on your needs
+    maxExportBatchSize: 10, // Adjust based on your needs
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+// Start the SDK
+sdk.start();
+
+console.log('Tracing initialized');
+
+module.exports = sdk;
